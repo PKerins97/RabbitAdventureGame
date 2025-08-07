4e2c46a36193a9e163efc12d0174f72408906b99
PKerins97 <paulkerins123@gmail.com>
2025-08-07 11:11:38 +0100

    updated player and camera scripts. Added animations Signed-off-by: PKerins97 <paulkerins123@gmail.com>

--- RabbitAdventureGame/Assets/Scripts/PlayerMovementScript.cs
+++ RabbitAdventureGame/Assets/Scripts/PlayerMovementScript.cs
@@ -8,18 +8,21 @@ public class PlayerMovementScript : MonoBehaviour
 
     public float moveSpeed = 5f; 
     public float sprintSpeed = 8f;
-    public float jumpForce = 10f;
+    public float jumpForce = 5f;
+    public float doubleJumpForce = 7f;
     public float rotationSpeed = 120f;
     public LayerMask groundMask;
     public float groundCheckDistance = 0.1f;
     public Transform cameraTransform;
 
     public int maxJumps = 2;
-    public float jumpCooldown = 0.01f;
+
+
+
 
     private Rigidbody rb;
     
-    private int jumpsCount;
+    private int jumpCount;
 
     public Animator animator;
     public float runSpeed = 5f;
@@ -28,6 +31,9 @@ public class PlayerMovementScript : MonoBehaviour
 
     bool isGrounded = true;
     bool isSprinting;
+    bool isFailing;
+    
+
     // Start is called before the first frame update
     void Start()
     {
@@ -57,6 +63,9 @@ public class PlayerMovementScript : MonoBehaviour
 
         bool isSprinting = moveZ > 0.1f && Input.GetKey(KeyCode.LeftShift);
         animator.SetBool("IsSprinting", isSprinting);
+
+
+
     }
     void FixedUpdate()
     {
@@ -103,17 +112,41 @@ public class PlayerMovementScript : MonoBehaviour
         }
     }
 
-
-
     void HandleJump()
     {
-        if (Input.GetButtonDown("Jump") && jumpsCount < maxJumps)
+        bool jumpPressed = Input.GetButtonDown("Jump");
+
+        // Ground check
+        isGrounded = Physics.Raycast(transform.position, Vector3.down, groundCheckDistance + 0.1f, groundMask);
+
+        if (isGrounded)
         {
-            rb.velocity = new Vector3(rb.velocity.x, 0f, rb.velocity.z); // reset Y velocity
-            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
-            jumpsCount++;
-            isGrounded = false;
+            jumpCount = 0;
+            animator.SetBool("IsFalling", false);
         }
+
+        if (jumpPressed && jumpCount < maxJumps)
+        {
+            rb.velocity = new Vector3(rb.velocity.x, 0f, rb.velocity.z); // reset vertical velocity
+
+            if (jumpCount == 0)
+            {
+                // First jump
+                rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+                animator.SetTrigger("Jump");
+            }
+            else if (jumpCount == 1)
+            {
+                // Second jump (double jump)
+                rb.AddForce(Vector3.up * doubleJumpForce, ForceMode.Impulse);
+                animator.SetTrigger("DoubleJump"); // <-- Different animation trigger
+            }
+
+            jumpCount++;
+        }
+
+        // Falling animation toggle
+        animator.SetBool("IsFalling", !isGrounded && rb.velocity.y < 0);
     }
 
     private void OnCollisionEnter(Collision collision)
@@ -122,54 +155,12 @@ public class PlayerMovementScript : MonoBehaviour
         if (collision.gameObject.tag == "Ground")
         {
             isGrounded = true;
-            jumpsCount = 0;
+            jumpCount = 0;
             maxJumps = 2;
+            animator.ResetTrigger("Jump");
+            animator.ResetTrigger("DoubleJump");
             Debug.LogError("Grounded = True");
         }
     }
 
-    //private void HandleMovement()
-
-    //{
-    //    float moveX = Input.GetAxis("Horizontal");
-    //    float moveZ = Input.GetAxis("Vertical");
-    //    Vector3 inputDirection = new Vector3(moveX, 0f, moveZ).normalized;
-    //    Vector3 move = new Vector3(moveX, 0f, moveZ).normalized;
-    //    Vector3 moveVelocity = move * moveSpeed;
-
-    //    // Apply movement to rigidbody
-    //    rb.velocity = new Vector3(moveVelocity.x, rb.velocity.y, moveVelocity.z);
-
-    //    if (animator != null)
-    //    {
-    //        animator.SetFloat("Horizontal", moveX);
-    //        animator.SetFloat("Vertical", moveZ);
-    //    }
-    //    if (inputDirection.magnitude >= 0.1f)
-    //    {
-    //        // Get camera's forward and right directions (flattened to XZ plane)
-    //        Vector3 cameraForward = cameraTransform.forward;
-    //        Vector3 cameraRight = cameraTransform.right;
-    //        cameraForward.y = 0f;
-    //        cameraRight.y = 0f;
-    //        cameraForward.Normalize();
-    //        cameraRight.Normalize();
-
-    //        // Convert input to world space based on camera orientation
-    //        Vector3 moveDirection = cameraForward * moveZ + cameraRight * moveX;
-    //        moveDirection.Normalize();
-
-    //        // Preserve current vertical velocity (for gravity/jumping)
-    //        Vector3 currentVelocity = rb.velocity;
-    //        rb.velocity = new Vector3(moveVelocity.x, currentVelocity.y, moveVelocity.z);
-
-
-    //    }
-    //    else
-    //    {
-    //        // Stop horizontal movement when no input
-    //        Vector3 currentVelocity = rb.velocity;
-    //        rb.velocity = new Vector3(0f, currentVelocity.y, 0f);
-    //    }
-    //}
 }
